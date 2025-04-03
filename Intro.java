3d78947a3348348406f33828a7b44eee0764df32
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-04-03 15:27:08 +0200

    Primo Programma Java

--- Intro.java
+++ Intro.java
@@ -5,3 +5,36 @@ di creare ,compilare, testare ed anche debaggare applicazioni Java.*/
 
 //Commento su singola linea
 /* Commento du multilinea */
+
+/*
+public class IlMioPrimoProjJava {
+
+ public static void main(String[] args) {
+  
+  
+  java.util.Scanner input = new java.util.Scanner(System.in); 
+  
+  System.out.println("Ciao come ti chiami");
+  String nome = input.nextLine();
+  
+  System.out.println("Ciao "+nome+". Molto piacere!");
+  System.out.println("E dimmi, "+nome+"... ma quanti anni hai?");
+  
+  
+  int anni = input.nextInt();
+  input.nextLine();
+  
+  System.out.println("OOOOk hai "+anni+" anni.");
+  
+  if (anni>=18) {
+   System.out.println("OK! Sei maggiorenne!");
+  }
+  else
+  {
+   System.out.println("OK! Sei minorenne!");
+  }
+  
+ }
+ 
+}
+*/
