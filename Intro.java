cf77ff3d390212ca406c19b5958fcd37e7e9b25e
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-04-14 14:53:39 +0200

    Array List 2.0

--- Intro.java
+++ Intro.java
@@ -1713,3 +1713,53 @@ public class ArrayList01 {
  }
 
 }
+
+package Dati;
+
+public class Persona {
+ private String nome;
+ private String cognome;
+ private int anni;
+ 
+ public Persona() {
+  this.nome = "";
+  this.cognome = "";
+  this.anni = 0;
+ }
+ 
+ public Persona(String nome, String cognome, int anni) {
+  this.nome = nome;
+  this.cognome = cognome;
+  this.anni = anni;
+ }
+ 
+ public String getNome() {
+  return nome;
+ }
+ 
+ public void setNome(String nome) {
+  this.nome = nome;
+ }
+ 
+ public String getCognome() {
+  return cognome;
+ }
+ 
+ public void setCognome(String cognome) {
+  this.cognome = cognome;
+ }
+ 
+ public int getAnni() {
+  return anni;
+ }
+ 
+ public void setAnni(int anni) {
+  this.anni = anni;
+ }
+ 
+ @Override
+ public String toString() {
+  return "Persona {nome=" + nome + ", cognome=" + cognome + ", anni=" + anni + "}";
+ }
+ 
+}
